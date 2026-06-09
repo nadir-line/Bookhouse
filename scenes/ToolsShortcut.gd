46243c0f5a9a007e2bfe88ed76bb884792f532b0
chickwithalmg-collab <chickwithalmg@gmail.com>
2026-06-09 15:01:38 +0200

    Finished second row of buyables
    Everyrthing works now yayyyy

--- scenes/ToolsShortcut.gd
+++ scenes/ToolsShortcut.gd
@@ -1,8 +1,7 @@
 extends SpinBox
 
-
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Books_Spinbox"):
+	if event.is_action_pressed("Tools_Spinbox"):
 		# Prevent input from doing other thingies
 		get_viewport().set_input_as_handled()
 		# SpinBox is made of multiple nodes, so we must grab focus on the LineEdit according to internjet
--- scripts/InterfaceMain.gd
+++ scripts/InterfaceMain.gd
@@ -10,6 +10,8 @@ var current_paper : float = 0
 var current_ink : float = 0
 var current_books : float = 0
 var current_wood : float = 0
+var current_metal : float = 0
+var current_tools : float = 0
 
 var funds_to_paper_ratio : float = 1
 var funds_to_ink_ratio : float = 1
@@ -20,8 +22,12 @@ var paper_to_books_ratio : float = 1
 var books_to_funds_ratio : float = 5
 
 var funds_to_wood_ratio : float = 2
-# var funds_to_wood_ratio : float = 1
+var funds_to_metal_ratio : float = 2
 
+var metal_to_tools_ratio : float = 1
+var wood_to_tools_ratio : float = 1
+
+var tools_to_funds_ratio : float = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -66,19 +72,52 @@ func _on_Make_Books_button_down():
 	$OptionsBooks/QtyBooks.value = 0
 
 func _on_Sell_Books_button_down():
-	current_books -= $OptionsBooks/QtyBooks.value
-	current_funds += $OptionsBooks/QtyBooks.value * books_to_funds_ratio
-
-func _on_Buy_Wood_button_down(): 
+	if(current_books >= 0):
+		current_books -= $OptionsBooks/QtyBooks.value
+		current_funds += $OptionsBooks/QtyBooks.value * books_to_funds_ratio
+	else: 
+		return
+		
+func _on_buy_wood_button_down(): 
 	if(current_funds >= 0):
 		current_funds -= $OptionsWood/QtyWood.value * funds_to_wood_ratio
 		current_wood += $OptionsWood/QtyWood.value
 	else:
 		return
 	$OptionsWood/QtyWood.value = 0
+	
+func _on_buy_metal_button_down(): 
+	if(current_funds >= 0):
+		current_funds -= $OptionsMetal/QtyMetal.value * funds_to_metal_ratio
+		current_metal += $OptionsMetal/QtyMetal.value
+	else:
+		return
+	$OptionsMetal/QtyMetal.value = 0
 
+func _on_make_tools_button_down():
+	if(current_metal && current_wood >= 0):
+		current_metal -= $OptionsTools/QtyTools.value * metal_to_tools_ratio
+		current_wood -= $OptionsTools/QtyTools.value * wood_to_tools_ratio
+		current_tools += $OptionsTools/QtyTools.value
+	else: 
+		return
+	$OptionsTools/QtyTools.value = 0
+
+func _on_sell_tools_button_down():
+	if(current_tools >= 0):
+		current_tools -= $OptionsTools/QtyTools.value
+		current_funds += $OptionsTools/QtyTools.value * tools_to_funds_ratio
+	else: 
+		return
+		
 func _on_Timer_timeout():
 	$CurrentFunds.text = "Funds: %0.2f" % [current_funds]
+	$CurrentFunds2.text = "Funds: %0.2f" % [current_funds]
+	
 	$CurrentInk.text = "Ink: %0.2f" % [current_ink]
 	$CurrentPaper.text = "Paper: %0.2f" % [current_paper]
 	$CurrentBooks.text = "Books: %0.2f" % [current_books]
+	
+	$CurrentWood.text = "Wood: %0.2f" % [current_wood]
+	$CurrentMetal.text = "Metal: %0.2f" % [current_metal]
+	$CurrentTools.text = "Tools: %0.2f" % [current_tools]
